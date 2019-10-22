e12cf4f67c651fa67164548504213e804da3930e
khoshmard <56787006+khoshmard@users.noreply.github.com>
2019-10-22 15:01:44 +0330

    Update base64.h

--- base64.h
+++ base64.h
@@ -1,6 +1,9 @@
-uint8_t b64invs[] = { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
-	59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5,
-	6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-	21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28,
-	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-	43, 44, 45, 46, 47, 48, 49, 50, 51 };
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+size_t b64_encoded_size(size_t inlen);
+int b64_encode(const unsigned char *in, size_t inlen,char *out, size_t outlen);
+int b64_isvalidchar(char c);
+size_t b64_decoded_size(const char *in);
+int b64_decode(const char *in, unsigned char *out, size_t outlen);
