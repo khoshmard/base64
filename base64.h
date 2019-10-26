e8e203f166beaca40a319f1e74f3a4bda2c44f25
khoshmard <56787006+khoshmard@users.noreply.github.com>
2019-10-26 10:16:40 +0330

    Update base64.h

--- base64.h
+++ base64.h
@@ -17,6 +17,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Public typedef ------------------------------------------------------------*/
 /* Public define -------------------------------------------------------------*/
 /* Public macro --------------------------------------------------------------*/
@@ -28,5 +32,9 @@ int b64_encode(const unsigned char *in, size_t inlen,char *out, size_t outlen);
 int b64_isvalidchar(char c);
 size_t b64_decoded_size(const char *in);
 int b64_decode(const char *in, unsigned char *out, size_t outlen);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __BASE64_H */
